ca253893c6c79588518b88f52607f96bb5649b47
Jens Harder <jens180980@gmail.com>
2023-10-16 13:56:39 +0200

    work on meet and mobile

--- mobile/src/App.js
+++ mobile/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h3> The mobile version of my portfolio is under construction</h3>
+      <h3>Please visit my portfolio from a desktop computer</h3>
+      <p>This site is being constructed with React JS Framework</p>
     </div>
   );
 }
