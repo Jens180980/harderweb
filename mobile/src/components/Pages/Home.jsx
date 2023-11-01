391514be20d49bf328ec53fc3c817bc1cee6fa67
Jens Harder <jens180980@gmail.com>
2023-11-01 12:01:51 +0100

    accordion big simple logic

--- mobile/src/components/Pages/Home.jsx
+++ mobile/src/components/Pages/Home.jsx
@@ -1,13 +1,15 @@
-import React from "react";
 import { Header } from "../Partials/Header/Header";
 import Footer from "../Partials/Footer/Footer";
 import { AccordionBig } from "../Partials/AccordionBig/AccordionBig";
+import "./Home.css";
 
 const Home = () => {
   return (
     <div>
       <Header pageTitle="Front Page" />
-      <AccordionBig />
+      <main className="home-main">
+        <AccordionBig />
+      </main>
       <Footer />
     </div>
   );
