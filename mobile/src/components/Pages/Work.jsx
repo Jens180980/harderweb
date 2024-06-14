63334011d0c50a48d3c43dc693d0246d256815c4
Jens Harder <jens180980@gmail.com>
2024-06-14 07:39:54 +0200

    mobile meet done

--- mobile/src/components/Pages/Work.jsx
+++ mobile/src/components/Pages/Work.jsx
@@ -1,8 +1,8 @@
 import Footer from "../Partials/Footer/Footer";
 import { Header } from "../Partials/Header/Header";
 import LikeMe from "../Partials/Likeme/LikeMe";
-import Slider from "../Partials/Slider/Slider";
 import { UnderConstruction } from "../Partials/UnderConstruction/UnderConstruction";
+import Gallery from "../Partials/Gallery/Gallery";
 import "./Work.css";
 
 const Work = () => {
@@ -11,9 +11,22 @@ const Work = () => {
       <Header pageTitle="work" />
       <section className="work-wrap">
         <p className="work-heading">Gallery coming soon...</p>
+        <Gallery />
         <div className="some">
-          <a href="https://github.com/Jens180980">github</a>
-          <a href="https://codepen.io/jens180980">codepen</a>
+          <a
+            href="https://github.com/Jens180980"
+            target="_blank"
+            rel="noreferrer"
+          >
+            github
+          </a>
+          <a
+            href="https://codepen.io/jens180980"
+            target="_blank"
+            rel="noreferrer"
+          >
+            codepen
+          </a>
         </div>
         <LikeMe />
       </section>
